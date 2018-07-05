bfdf0c23db755e45f690430f257573888a1bc2b6
Alexandre Zelante <40640392+AlexandreZelante@users.noreply.github.com>
2018-07-05 04:20:52 -0300

    Add files via upload

---
+++ Default.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace myWaiter
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["conta"] != null && Session["idMesa"] != null)
+            {
+                if(Session["currentUser"] != null)
+                {
+                    if (Session["currentUser"].ToString() == "Mesa " + Session["idMesa"].ToString())
+                    {
+                        Response.Redirect("~/Mobile/Pages/produtosCategoria.aspx");
+                    }
+                    else
+                    {
+                        Response.Redirect("~/Mobile/Pages/paginaComandas.aspx");
+                    }
+                }
+            }
+            else
+            {
+                Response.Redirect("~/Admin/Home/Home.aspx");
+            }    
+        }
+    }
+}
